02cf736d64fedf9aacaf1062527ac34bff319033
felipe torres <felipe.torressepulveda@gmail.com>
2016-05-01 23:53:58 -0300

    update

--- app/components/Lyrics/Lyrics.js
+++ app/components/Lyrics/Lyrics.js
@@ -5,8 +5,6 @@ import React, { Component } from 'react';
 export default class Home extends Component {
   render() {
     let Lyric = '';
-    console.log(this.props.songSentences[this.props.beat])
-    console.log(this.props.songSentences)
     if (this.props.songSentences[this.props.beat]) {
       Lyric += ' ' + this.props.songSentences[this.props.beat].text
     }
